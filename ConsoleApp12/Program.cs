6afd88f4fe020f28af143ee19528859310fab75a
yevhen_luchko@epam.com <yevhen_luchko@epam.com>
2024-12-10 09:17:45 +0200

    44

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -2,4 +2,7 @@
 Console.WriteLine("Hello, World!1");
 Console.WriteLine("Hello, World!2");
 Console.WriteLine("Hello, World!3");
+<<<<<<< HEAD
 Console.WriteLine("Hello, World!4");
+=======
+>>>>>>> d1c79e8 (3)
