0ce0f6b569b331d2f91ca68e03a246f6d80a53c3
yevhen_luchko@epam.com <yevhen_luchko@epam.com>
2025-02-07 12:06:23 +0200

    комментарий к коммиту

--- ConsoleApp12/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!1");
-Console.WriteLine("Hello, World!2");
-Console.WriteLine("Hello, World!5");
-
-//t
-//q
