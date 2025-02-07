43dd43d67c9fa72430ac9838fc62e903df208750
yevhen_luchko@epam.com <yevhen_luchko@epam.com>
2025-02-07 12:15:23 +0200

    qwerfg

--- ConsoleApp17/Program.cs
+++ ConsoleApp17/Program.cs
@@ -1,10 +1,36 @@
 ﻿namespace ConsoleApp17
 {
-    internal class Program
+    public class Program
     {
+        protected Program()
+            {
+                    
+            }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MyClass myClass = new MyClass();
+            myClass.F(q =>
+            {
+                q.Q1();
+                q.Q();
+            });
         }
     }
-}
+
+
+    class MyClass
+        {
+        B b=new B();
+
+       public void F(Action<B> action) {
+            action.Invoke(b);
+        }
+
+        }
+
+    internal class B
+        {
+        public void Q() { }
+        public void Q1() { }
+        }
+    }
